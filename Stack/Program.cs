28bc7cd3b2ddf056570489a79ac0cbe4a7768f25
Tannaz <tannaz.fathi@yahoo.com>
2024-07-26 18:39:07 +0430

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(10);
+            for (int i = 0; i < 3; i++)
+            {
+                stack.Push("blachers");
+                stack.Push("beesesr");
+                stack.Push("feels");
+            }
+            stack.Pop();
+            stack.Peek();
+
+            while (!stack.isEmpty())
+            {
+                var var = stack.Pop();
+                Console.WriteLine(var);
+            }
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        public int MaxSize { get; set; }
+        public string[] StackArray { get; set; }
+        public int Top {  get; set; }
+
+        public Stack(int size)
+        {
+            this.MaxSize= size;
+            //creat Array with size
+            this.StackArray= new string[size];
+            //becaus of 0 index
+            this.Top = -1;
+
+        }
+
+        public void Push(string item)
+        {
+            Top++;
+            StackArray[Top] = item;
+
+        }
+
+        public string Pop()
+        {
+            int old_top = Top;
+            Top--;
+            return StackArray[old_top];
+        }
+
+        public string Peek()
+        {
+            return StackArray[Top];
+        }
+
+        public bool isEmpty()
+        {
+            return Top == 0;
+        }
+        public bool isFull()
+        {
+            return (MaxSize-1==Top);
+        }
+
+
+
+
+
+
+    }
+}
